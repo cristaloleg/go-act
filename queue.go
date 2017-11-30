01fe1d032b0364f173771654a56179277df3437f
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-11-30 18:11:07 +0100

    queue: extract goroutine to func

--- queue.go
+++ queue.go
@@ -1,35 +1,19 @@
 package act
 
 type Queue struct {
-	in  chan interface{}
-	out chan interface{}
+	in   chan interface{}
+	out  chan interface{}
+	done chan struct{}
 }
 
 func NewQueue() *Queue {
 	q := &Queue{
-		in:  make(chan interface{}),
-		out: make(chan interface{}),
+		in:   make(chan interface{}),
+		out:  make(chan interface{}),
+		done: make(chan struct{}),
 	}
 	go func() {
-		var last interface{}
-		var buf []interface{}
 
-		last = <-q.in
-
-		for {
-			select {
-			case value := <-q.in:
-				buf = append(buf, value)
-
-			case q.out <- last:
-				sz := len(buf)
-				if sz == 0 {
-					last = <-q.in
-				} else {
-					last, buf = buf[0], buf[1:]
-				}
-			}
-		}
 	}()
 	return q
 }
@@ -40,4 +24,33 @@ func (q *Queue) Push() chan<- interface{} {
 
 func (q *Queue) Pop() <-chan interface{} {
 	return q.out
+}
+
+func (q *Queue) Close() {
+	q.done <- struct{}{}
+}
+
+func (q *Queue) run() {
+	var last interface{}
+	var buf []interface{}
+
+	last = <-q.in
+
+	for {
+		select {
+		case value := <-q.in:
+			buf = append(buf, value)
+
+		case q.out <- last:
+			sz := len(buf)
+			if sz == 0 {
+				last = <-q.in
+			} else {
+				last, buf = buf[0], buf[1:]
+			}
+
+		case <-q.done:
+			return
+		}
+	}
 }
