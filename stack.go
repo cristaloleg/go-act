defa49749e042c67ebdd9435c3642fa4d766518b
Oleg Kovalov <iamolegkovalov@gmail.com>
2017-11-30 18:09:59 +0100

    stack: extract goroutine to func

--- stack.go
+++ stack.go
@@ -1,37 +1,18 @@
 package act
 
 type Stack struct {
-	in  chan interface{}
-	out chan interface{}
+	in   chan interface{}
+	out  chan interface{}
+	done chan struct{}
 }
 
 func NewStack() *Stack {
 	s := &Stack{
-		in:  make(chan interface{}),
-		out: make(chan interface{}),
+		in:   make(chan interface{}),
+		out:  make(chan interface{}),
+		done: make(chan struct{}),
 	}
-	go func() {
-		var last interface{}
-		var buf []interface{}
-
-		last = <-s.in
-
-		for {
-			select {
-			case value := <-s.in:
-				buf = append(buf, last)
-				last = value
-
-			case s.out <- last:
-				sz := len(buf)
-				if sz == 0 {
-					last = <-s.in
-				} else {
-					last, buf = buf[sz-1], buf[:sz-1]
-				}
-			}
-		}
-	}()
+	go s.run()
 	return s
 }
 
@@ -41,4 +22,34 @@ func (s *Stack) Push() chan<- interface{} {
 
 func (s *Stack) Pop() <-chan interface{} {
 	return s.out
+}
+
+func (s *Stack) Close() {
+	s.done <- struct{}{}
+}
+
+func (s *Stack) run() {
+	var last interface{}
+	var buf []interface{}
+
+	last = <-s.in
+
+	for {
+		select {
+		case value := <-s.in:
+			buf = append(buf, last)
+			last = value
+
+		case s.out <- last:
+			sz := len(buf)
+			if sz == 0 {
+				last = <-s.in
+			} else {
+				last, buf = buf[sz-1], buf[:sz-1]
+			}
+
+		case <-s.done:
+			return
+		}
+	}
 }
